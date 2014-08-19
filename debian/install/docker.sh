ce27c479e4d347ded47cbe2e6b1df10c7bb0b381
George <george.yord@gmail.com>
2014-08-19 18:13:32 +0300

    Fix docekr script

--- debian/install/docker.sh
+++ debian/install/docker.sh
@@ -14,17 +14,16 @@ curl -s https://get.docker.io/ubuntu/ | sh
 # Add the docker group if it doesn't already exist.
 groupadd docker
 
+
 # Add the connected user "${USER}" to the docker group.
 # Change the user name to match your preferred user.
 # You may have to logout and log back in again for
 # this to take effect.
-gpasswd -a ${USER} docker
-gpasswd -a ${SUDO_USER} docker
+DCK_USER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
+gpasswd -a ${DCK_USER} docker
 
 # Add current user to the docker group to run docker commands without sudo
-usermod -aG docker $USER
-usermod -aG docker $SUDO_USER
+usermod -aG docker $DCK_USER
 
 # Restart the Docker daemon.
-# If you are in Ubuntu 14.04, use docker.io instead of docker
 service docker restart
