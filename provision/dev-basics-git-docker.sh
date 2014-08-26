d8dc65eedd15dc4d6562ad7aa74df50d0fcf5045
Giorgos Papaefthimiou <giorgos+github@peopleperhour.com>
2014-08-26 16:43:02 +0300

    Update dev-basics-git-docker.sh

--- provision/dev-basics-git-docker.sh
+++ provision/dev-basics-git-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir ~/bin
 wget -qO- https://raw.githubusercontent.com/georgeyord/scripts/master/provision/provision.sh > /tmp/provision.sh && chmod a+x /tmp/provision.sh
 
 sudo /tmp/provision.sh distro-upgrade basic-tools nodejs git docker apt-cleanup disk-space
