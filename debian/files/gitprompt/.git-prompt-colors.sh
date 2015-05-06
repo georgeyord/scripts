e03fad4429be61b69fd6bb2c9045f4318644c0ee
Giorgos Papaefthimiou <giorgos+github@peopleperhour.com>
2015-05-06 17:44:13 +0300

    Fix for new version

--- debian/files/gitprompt/.git-prompt-colors.sh
+++ debian/files/gitprompt/.git-prompt-colors.sh
@@ -5,6 +5,10 @@
 override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Custom"
 
+  GIT_PROMPT_STAGED="${Yellow}●"
+  GIT_PROMPT_STASHED="${BoldMagenta}⚑ "
+  GIT_PROMPT_CLEAN="${Green}✔"
+  
   #GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${USER}@${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_START_USER="${Blue}[${USER}] _LAST_COMMAND_INDICATOR_ ${Yellow}${PathShort}${ResetColor}"
   #GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_ ${USER}@${GIT_PROMPT_START_USER}"
@@ -13,3 +17,5 @@ override_git_prompt_colors() {
   GIT_PROMPT_END_USER="${Blue}$ ${ResetColor}"
   GIT_PROMPT_END_ROOT="${GIT_PROMPT_END_USER}"
 }
+
+reload_git_prompt_colors "Custom"
