0e3180181bcca0029c722054bae96b635393d1ad
George Yord <george.yord@gmail.com>
2015-09-16 11:17:21 +0300

    Introduce atom, bin scripts, vncserver and changed i3 configuration

--- debian/install/vncserver.sh
+++ debian/install/vncserver.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Install and configure vncserver to gain remote access to the graphical interface of the machine"
-echo "Run 'vncserver' or 'vncserver.sh' to start the service"
 
 apt-get update -y -qq
 apt-get install --assume-yes --force-yes -qq \
@@ -9,5 +8,8 @@ apt-get install --assume-yes --force-yes -qq \
 
 # Add 'vncserver' script to executable PATH
 downloadExecutableToBin https://raw.githubusercontent.com/georgeyord/scripts/master/debian/files/vncserver/vncserver.sh vncserver
+
+echo "Run 'vncserver' or 'vncserver.sh' to start the service"
+sleep 1
 
 unset FLAG_INTERACTIVE
