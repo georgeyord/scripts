d0729fc68909cc0711aa6da1e38a2932f4ccac94
George Yord <george.yord@gmail.com>
2015-10-06 13:56:21 +0300

    Introduce gitpr helper script

--- debian/files/git/bin/gitpr.sh
+++ debian/files/git/bin/gitpr.sh
@@ -3,41 +3,38 @@
 # USAGE: gitpr
 # EXAMPLE: gitpr
 
+function uncommitedFiles() {
+  git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l
+}
+
 CURRENT_PATH=`pwd`
 
 # If there are no arguments, run for current folder
 if [ -z "$1" ]; then
-    $1='.'
+    set -- "."
 fi
 
 while test ${#} -gt 0
 do
   if [ -d "$1" ]; then
     cd "$1"
-    gitpr
+
+    UNCOMMITED_FILES=$(uncommitedFiles)
+    echo -e "\n==> Current branch for `pwd`: `git rev-parse --abbrev-ref HEAD` ($UNCOMMITED_FILES uncommited files)\n"
+
+    if [[ ! $UNCOMMITED_FILES == 0 ]]; then
+      git stash && echo -e "\n***\n"
+    fi
+
+    git pull --rebase
+
+    if [[ ! $UNCOMMITED_FILES == 0 ]]; then
+      echo -e "\n***\n" && git stash pop
+    fi
+
     cd "$CURRENT_PATH"
   else
     echo "Directory '${1}' not found"
   fi
   shift
 done
-
-
-function uncommitedFiles() {
-  git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l
-}
-
-function gitpr() {
-  UNCOMMITED_FILES=$(uncommitedFiles)
-  echo -e "\n==> Current branch for `pwd`: `git rev-parse --abbrev-ref HEAD` ($UNCOMMITED_FILES uncommited files)\n"
-
-  if [[ ! $UNCOMMITED_FILES == 0 ]]; then
-    git stash && echo -e "\n***\n"
-  fi
-
-  git pull --rebase
-
-  if [[ ! $UNCOMMITED_FILES == 0 ]]; then
-    echo -e "\n***\n" && git stash pop
-  fi
-}
