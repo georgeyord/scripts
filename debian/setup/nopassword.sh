5f8749daa0dc5b953e1bcf32862f9b5edd20df94
Giorgos Papaefthimiou <georgeyord@users.noreply.github.com>
2015-11-18 14:28:23 +0200

    Update nopassword.sh

--- debian/setup/nopassword.sh
+++ debian/setup/nopassword.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo echo "%$USER ALL=NOPASSWD:ALL" > "/etc/sudoers.d/$USER" && sudo chmod 0440 /etc/sudoers.d/$USER && sudo usermod -a -G sudo $USER
+sudo bash -c "echo '%$USER ALL=NOPASSWD:ALL' > '/etc/sudoers.d/$USER'" && sudo chmod 0440 /etc/sudoers.d/$USER && sudo usermod -a -G sudo $USER
