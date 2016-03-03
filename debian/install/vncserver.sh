d531749e022e98c8f0d386ebcf9e74a6bcf494bd
George Yord <george.yord@gmail.com>
2016-03-03 10:33:06 +0200

    Change VNCserver in fovar of tightvnc+i3wm

--- debian/install/vncserver.sh
+++ debian/install/vncserver.sh
@@ -4,12 +4,26 @@ echo "Install and configure vncserver to gain remote access to the graphical int
 
 apt-get update -y -qq
 apt-get install --assume-yes --force-yes -qq \
-	x11vnc
+	xfce4 xfce4-goodies tightvncserver
 
-# Add 'vncserver' script to executable PATH
-downloadExecutableToBin https://raw.githubusercontent.com/georgeyord/scripts/master/debian/files/vncserver/vncserver.sh vncserver
+mkdir -p $DEFAULT_USER_PATH/.vnc
 
-echo "Run 'vncserver' or 'vncserver.sh' to start the service"
+wget -qO- https://raw.githubusercontent.com/georgeyord/scripts/master/debian/files/vncserver/xstartup > $DEFAULT_USER_PATH/.vnc/xstartup
+wget -qO- https://raw.githubusercontent.com/georgeyord/scripts/master/debian/files/vncserver/xstartup.xfce > $DEFAULT_USER_PATH/.vnc/xstartup.xfce
+
+if [[ $INTERACTIVE == 0 ]]; then
+    FLAG_INTERACTIVE=0
+else
+    FLAG_INTERACTIVE=$(yesNo  "Do you wish to set the VNC password?")
+fi
+
+if [[ $FLAG_INTERACTIVE == 1 ]]; then
+    vncpasswd
+else
+	echo "Run 'vncpasswd' to set the VNC password."
+fi
+
+echo "Run 'vncserver' to start the VNC server"
 sleep 1
 
 unset FLAG_INTERACTIVE
