09e88c0f5aeb9b74246f78a216cf1a55107ebc1e
George Yord <george.yord@gmail.com>
2016-03-09 16:50:20 +0200

    Introduce Parcellite - a clipboard manager

---
+++ debian/install/parcellite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Parcellite - a clipboard manager
+# Reference: http://parcellite.sourceforge.net/
+echo "Install Parcellite - a clipboard manager"
+
+apt-get install --assume-yes --force-yes -qq \
+	parcellite
