400d9637fe086d99e6fbb4ff986e252caf29fcd6
George Yord <george.yord@gmail.com>
2016-04-27 13:07:49 +0300

    Use saveLocallyFromUrl where possible

--- install/docker.sh
+++ install/docker.sh
@@ -30,21 +30,21 @@ groupadd docker
 gpasswd -a $DEFAULT_USER docker
 
 echo "Install Docker compose"
-curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+saveLocallyFromUrl https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ensureAppExists "docker-compose"
-curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+/usr/local/bin/docker-compose https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose /etc/bash_completion.d/docker-compose
 
 echo "Install Docker compose bash completion"
-curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+saveLocallyFromUrl https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose /etc/bash_completion.d/docker-compose
 
 echo "Install Docker machine"
-curl -L https://github.com/docker/machine/releases/download/${DOCKER_MACHINE_VERSION}/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine
+saveLocallyFromUrl https://github.com/docker/machine/releases/download/${DOCKER_MACHINE_VERSION}/docker-machine-`uname -s`-`uname -m` /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 ensureAppExists "docker-machine"
 
 echo "Install Kubernetes's kubectl"
-curl -L https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl > /usr/local/bin/kubectl && \
+saveLocallyFromUrl https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl /usr/local/bin/kubectl && \
 chmod +x /usr/local/bin/kubectl
 ensureAppExists "kubectl"
 
--- install/gitprompt.sh
+++ install/gitprompt.sh
@@ -5,14 +5,15 @@ echo "More details here: https://github.com/magicmonty/bash-git-prompt"
 
 git clone https://github.com/magicmonty/bash-git-prompt.git $DEFAULT_USER_PATH/.bash-git-prompt
 
-wget -qO- ${REPO_SCRIPT_PATH}/gitprompt/.git-prompt-colors.sh > $DEFAULT_USER_PATH/.git-prompt-colors.sh
+saveLocallyFromUrl ${REPO_SCRIPT_PATH}/gitprompt/.git-prompt-colors.sh $DEFAULT_USER_PATH/.git-prompt-colors.sh
 
 # Set ownership to the default user
 mkdir -p $DEFAULT_USER_PATH/.bash-git-prompt
 chown -R $DEFAULT_USER:$DEFAULT_USER_GROUP $DEFAULT_USER_PATH/.bash-git-prompt
 chown -R $DEFAULT_USER:$DEFAULT_USER_GROUP $DEFAULT_USER_PATH/.git-prompt-colors.sh
 
-if grep -Fxq "$DEFAULT_USER_PATH/.bashrc" "gitprompt"; then
+grep -Fxq "$DEFAULT_USER_PATH/.bashrc" "gitprompt" > /dev/null
+if [[ $? -ne 0 ]] ; then
     echo "GitPrompt already included in bashrc"
 else
     echo "GIT_PROMPT_THEME=Custom # use custom .git-prompt-colors.sh" >> $DEFAULT_USER_PATH/.bashrc
