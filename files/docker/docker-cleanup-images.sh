dce1a145f66c24e643b2971b8ce2af15ad5d8799
George Yord <george.yord@gmail.com>
2016-09-02 11:29:33 +0300

    Fix docker-cleanup-images

--- files/docker/docker-cleanup-images.sh
+++ files/docker/docker-cleanup-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Clean up un-tagged docker images:"
-docker images -q --filter "dangling=true" | xargs --no-run-if-empty docker volume rmi
+docker images -q --filter "dangling=true" | xargs --no-run-if-empty docker rmi -f
