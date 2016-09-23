e51083c9597576f5ec6cd8c2ad2ad6083bc38758
George Yord <george.yord@gmail.com>
2016-09-23 13:01:15 +0300

    Fix permissions problem in vscode extensions

--- install/vscode.sh
+++ install/vscode.sh
@@ -11,32 +11,32 @@ ensureAppExists "code"
 npm install -g typings
 ensureAppExists "typings"
 
-code --install-extension DotJoshJohnson.xml
-code --install-extension HookyQR.beautify
-code --install-extension Kasik96.format-indent
-code --install-extension PeterJausovec.vscode-docker
-code --install-extension Shan.code-settings-sync
-code --install-extension TwentyChung.jsx
-code --install-extension Zignd.html-css-class-completion
-code --install-extension abusaidm.html-snippets
-code --install-extension alefragnani.project-manager
-code --install-extension christian-kohler.path-intellisense
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dbankier.vscode-instant-markdown
-code --install-extension donjayamanne.githistory
-code --install-extension felixfbecker.php-debug
-code --install-extension humao.rest-client
-code --install-extension ikappas.phpcs
-code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
-code --install-extension mohsen1.prettify-json
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension qinjia.view-in-browser
-code --install-extension rebornix.Ruby
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension robinbentley.sass-indented
-code --install-extension vsmobile.vscode-react-native
-code --install-extension waderyan.gitblame
-code --install-extension xabikos.JavaScriptSnippets
+su $DEFAULT_USER -c "code --install-extension DotJoshJohnson.xml"
+su $DEFAULT_USER -c "code --install-extension HookyQR.beautify"
+su $DEFAULT_USER -c "code --install-extension Kasik96.format-indent"
+su $DEFAULT_USER -c "code --install-extension PeterJausovec.vscode-docker"
+su $DEFAULT_USER -c "code --install-extension Shan.code-settings-sync"
+su $DEFAULT_USER -c "code --install-extension TwentyChung.jsx"
+su $DEFAULT_USER -c "code --install-extension Zignd.html-css-class-completion"
+su $DEFAULT_USER -c "code --install-extension abusaidm.html-snippets"
+su $DEFAULT_USER -c "code --install-extension alefragnani.project-manager"
+su $DEFAULT_USER -c "code --install-extension christian-kohler.path-intellisense"
+su $DEFAULT_USER -c "code --install-extension dbaeumer.vscode-eslint"
+su $DEFAULT_USER -c "code --install-extension dbankier.vscode-instant-markdown"
+su $DEFAULT_USER -c "code --install-extension donjayamanne.githistory"
+su $DEFAULT_USER -c "code --install-extension felixfbecker.php-debug"
+su $DEFAULT_USER -c "code --install-extension humao.rest-client"
+su $DEFAULT_USER -c "code --install-extension ikappas.phpcs"
+su $DEFAULT_USER -c "code --install-extension lonefy.vscode-JS-CSS-HTML-formatter"
+su $DEFAULT_USER -c "code --install-extension mohsen1.prettify-json"
+su $DEFAULT_USER -c "code --install-extension msjsdiag.debugger-for-chrome"
+su $DEFAULT_USER -c "code --install-extension qinjia.view-in-browser"
+su $DEFAULT_USER -c "code --install-extension rebornix.Ruby"
+su $DEFAULT_USER -c "code --install-extension robertohuertasm.vscode-icons"
+su $DEFAULT_USER -c "code --install-extension robinbentley.sass-indented"
+su $DEFAULT_USER -c "code --install-extension vsmobile.vscode-react-native"
+su $DEFAULT_USER -c "code --install-extension waderyan.gitblame"
+su $DEFAULT_USER -c "code --install-extension xabikos.JavaScriptSnippets"
 
 # typings install dt~backbone --global
 # typings install lodash --save
