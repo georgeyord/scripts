a46f15d253438da9f8fdc5845fd92baf2094e636
Giorgos Papaefthimiou <georgeyord@users.noreply.github.com>
2016-11-21 13:51:34 +0200

    Update docker-bin-alias.sh

--- files/docker-bin/docker-bin-alias.sh
+++ files/docker-bin/docker-bin-alias.sh
@@ -2,5 +2,5 @@
 
 DOCKER_BIN_TEST_PORT=18080
 alias docker-bin-test='echo "Open in browser: http://localhost:${DOCKER_BIN_TEST_PORT}" && docker run --rm -it -p ${DOCKER_BIN_TEST_PORT}:8000 -t jwilder/whoami'
-alias docker-server-static='echo "Serving "${1:-default `pwd`}" at : http://localhost:${2:-default 8088}" && docker run -it --rm --volume="${1:-default `pwd`}:/static:ro" -p="${2:-default 8088}:80" georgeyord/static-file-server-nginx'
+alias docker-server-static='echo "Serving "${1:-`pwd`}" at : http://localhost:${2:-8088}" && docker run -it --rm --volume="${1:-`pwd`}:/static:ro" -p="${2:-8088}:80" georgeyord/static-file-server-nginx'
 alias htpasswd='docker run --rm -ti crosbymichael/htpasswd'
