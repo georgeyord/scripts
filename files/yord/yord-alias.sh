824c937b488370b61743c641d2ffa165232513d7
George Yord <george.yord@gmail.com>
2017-02-23 09:05:25 +0200

    Minor change in yord-alias

--- files/yord/yord-alias.sh
+++ files/yord/yord-alias.sh
@@ -7,7 +7,7 @@ alias regui='sudo service restart lightdm'
 
 alias 24='cd /opt/codebase/tripsta/app && which term-title && term-title 24'
 alias frontline='cd /opt/codebase/tripsta/frontline && which term-title && term-title fl'
-alias bolt='cd /opt/codebase/tripsta/bolt && which term-title && term-title bolt'
+alias bolt='cd /opt/codebase/tripsta/bolt/application && which term-title && term-title bolt'
 alias bender='cd /opt/codebase/tripsta/bender && which term-title && term-title bolt'
 alias tpdocker='cd /opt/codebase/tripsta/docker/compose && which term-title && term-title tpdck'
 alias codebase='cd /opt/codebase'
