2578fa5e0ef71f0f1b9e43f2fc21846ca4c3c5e1
Giorgos Papaefthimiou <georgeyord@users.noreply.github.com>
2017-10-26 11:32:09 +0300

    Update sublime.sh

--- install/sublime.sh
+++ install/sublime.sh
@@ -2,12 +2,13 @@
 
 echo -e "\n*** Install Sublime 3"
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-addRepo '^deb https://download.sublimetext.com/ apt/stable/' "deb https://download.sublimetext.com/ apt/stable/"
+add-apt-repository ppa:webupd8team/sublime-text-3
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+# addRepo '^deb https://download.sublimetext.com/ apt/stable/' "deb https://download.sublimetext.com/ apt/stable/"
 apt-get update -qq
 
 apt-get install --assume-yes --force-yes -qq \
-        sublime-text
+        sublime-text-installer
 
 ensureAppExists "subl" "sublime"
 
